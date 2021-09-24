f5fa678cc16f073b9c88c72f01d7e9f29a3f118c
santiagojordan <sjordan@udesa.edu.ar>
2021-09-24 10:42:39 -0300

    Correccion y cambio de npm packages

--- myapp/controllers/miPerfilController.js
+++ myapp/controllers/miPerfilController.js
@@ -2,11 +2,13 @@ const dataUsuarios = require ("../data/usuarios");
 const dataPosteos = require ("../data/posteos");
 const dataComentarios = require ("../data/comentarios");
 
-const usersController = {
-    index: (req,res) => {
-        
-    
+const miPerfilController = {
+    index: function (req,res) {
+        res.render("miPerfil", {users: data.lista, listaPosts: posts.lista})
+    },
+    edit: function (req,res) {
+        res.render("editarPerfil")
     }
 }
 
-module.exports = indexController;
+module.exports = miPerfilController;
