81a68a15145e6de32977b70a63b7661dcb0bb9ff
santiagojordan <sjordan@udesa.edu.ar>
2021-09-26 17:13:56 -0300

    data posteos

---
+++ myapp/data/posteos.js
@@ -0,0 +1,77 @@
+const post = {
+    lista: [
+    {
+        id: 1,
+        idUsuario: 1,
+        foto: "./images/audir8.jpeg",
+        pie_de_foto: "Que buen auto!",
+        fecha: 2021/09/01,
+    },
+
+    {
+        id: 2,
+        idUsuario: 2,
+        foto:"./images/bentleyBentayga.jpeg" ,
+        pie_de_foto: "Que linda camioneta!",
+        fecha: 2021/09/14,
+    },
+
+    {
+        id: 3,
+        idUsuario: 3,
+        foto:"./images/audiA4.jpeg" ,
+        pie_de_foto: "Que buen Audi ese!",
+        fecha: 2021/08/31,
+    },
+
+    {
+        id: 4,
+        idUsuario: 4,
+        foto:"./images/bmwSerie4.jpeg" ,
+        pie_de_foto: "Como me gusta ese BMW!",
+        fecha: 2021/09/15,
+    },
+
+    {
+        id: 5,
+        idUsuario: 5,
+        foto:"./images/lamborghiniUrus.jpeg" ,
+        pie_de_foto: "Que nave, es un sueño!",
+        fecha: 2021/09/02,
+    },
+
+    {
+        id: 6,
+        idUsuario: 6,
+        foto:"./images/porsche911TurboS.jpeg" ,
+        pie_de_foto: "Ese auto es perfecto!",
+        fecha: 2021/08/31,
+    },
+
+    {
+        id: 7,
+        idUsuario: 7,
+        foto:"./images/porscheCayenne.jpeg" ,
+        pie_de_foto: "Es la camioneta ideal!",
+        fecha: 2021/09/09,
+    },
+
+    {
+        id: 8,
+        idUsuario: 8,
+        foto:"./images/ramTrx.jpeg" ,
+        pie_de_foto: "Es una bestia, es increible!",
+        fecha: 2021/09/11,
+    },
+
+    {
+        id: 9,
+        idUsuario: 9,
+        foto:"./images/teslaModelX.jpeg" ,
+        pie_de_foto: "Que linda camioneta!",
+        fecha: 2021/09/14,
+    },
+
+
+    ]
+}
