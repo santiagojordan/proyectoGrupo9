2998c6ea147444268ae7d0f99bc7b1f16b926c22
CamiWagner <camilawagnerbuldra@hotmail.com>
2021-09-26 20:31:34 -0300

    usuarios

---
+++ myapp/data/usuarios.js
@@ -0,0 +1,54 @@
+const usuarios = {
+   lista: [
+       {
+           id: 1,
+           nombre: "Ana",
+           apellido: "Folmer",
+           email: "anafolmer@gmail.com",
+           contrasenia: 1265,
+           fotoPerfil: "",
+           fecha: 2021-08-30,
+           telefono: 343620957    
+       },
+       {
+        id: 2,
+        nombre: "Mauro",
+        apellido: "Silva",
+        email: "maurosilva@gmail.com",
+        contrasenia: 2303,
+        fotoPerfil: "",
+        fecha: 2021-09-01,
+        telefono: 343487109   
+       },
+       {
+        id: 3,
+        nombre: "Martín",
+        apellido: "Schmidt",
+        email: "martinschmidt@gmail.com",
+        contrasenia: 4537,
+        fotoPerfil: "",
+        fecha: 2021-09-05,
+        telefono: 343290716    
+       },
+       {
+        id: 4,
+        nombre: "Carolina",
+        apellido: "Lell",
+        email: "carolinalell@gmail.com",
+        contrasenia: 1117,
+        fotoPerfil: "",
+        fecha: 2021-08-31,
+        telefono: 343265487    
+       },
+       {
+        id: 5,
+        nombre: "Muriel",
+        apellido: "Gorostiaga",
+        email: "murielgorostiaga@gmail.com",
+        contrasenia: 0456,
+        fotoPerfil: "",
+        fecha: 2021-09-10,
+        telefono: 343653448    
+       }
+    ]
+}
