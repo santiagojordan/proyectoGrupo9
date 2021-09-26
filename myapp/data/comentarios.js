2a97d5c18e69e3daec2ce0efbe796f4d85bd8e4e
CamiWagner <camilawagnerbuldra@hotmail.com>
2021-09-26 17:15:16 -0300

    comentarios incompleto

---
+++ myapp/data/comentarios.js
@@ -0,0 +1,137 @@
+const comentarios = {
+    lista: [
+        {
+            id: 1,
+            idUsuario: 2,
+            idPost: 8,
+            comentario: "Que lindo auto!",
+            fechaCreacion: 2021-09-11
+        },
+        {
+            id: 2,
+            idUsuario: 3,
+            idPost: 8,
+            comentario: "Hola, me dicen el precio?",
+            fechaCreacion: 2021-09-11
+        }, 
+        {
+            id: 3,
+            idUsuario: 1,
+            idPost: 8,
+            comentario: "Uff que buen color!",
+            fechaCreacion: 2021-09-11
+        },
+        {
+            id: 4,
+            idUsuario: 4,
+            idPost: 8,
+            comentario: "Tienen fotos del interior del auto?",
+            fechaCreacion: 2021-09-12
+        },
+        {
+            id: 5,
+            idUsuario: 1,
+            idPost: 9,
+            comentario: "Que buen auto!",
+            fechaCreacion: 2021-09-15
+        },
+        {
+            id: 6,
+            idUsuario: 1,
+            idPost: 9,
+            comentario: "Me dicen el precio?",
+            fechaCreacion: 2021-09-15
+        },
+        {
+            id: 7,
+            idUsuario: 4,
+            idPost: 9,
+            comentario: "Viene en otros colores?",
+            fechaCreacion: 2021-09-15
+        },
+        {
+            id: 8,
+            idUsuario: 1,
+            idPost: 9,
+            comentario: "Que métodos de pago manejan?",
+            fechaCreacion: 2021-09-16
+        },
+        {
+            id: 9,
+            idUsuario: 5,
+            idPost: 10,
+            comentario: "Que lindo auto!",
+            fechaCreacion: 2021-09-17
+        },
+        {
+            id: 10,
+            idUsuario: 2,
+            idPost: 10,
+            comentario: "Que precio tiene?",
+            fechaCreacion: 2021-09-17
+        },
+        {
+            id: 11,
+            idUsuario: 4,
+            idPost: 10,
+            comentario: "Hermoso color!",
+            fechaCreacion: 2021-09-18
+        },
+        {
+            id: 12,
+            idUsuario: 3,
+            idPost: 10,
+            comentario: "Tienen más fotos del vehículo?",
+            fechaCreacion: 2021-09-18
+        },
+        {
+            id: 13,
+            idUsuario: 1,
+            idPost: 1,
+            comentario: "Muy lindo auto",
+            fechaCreacion: 2021-09-01
+        },
+        {
+            id: 14,
+            idUsuario: 3,
+            idPost: 1,
+            comentario: "Puedo pasar a ver el auto?",
+            fechaCreacion: 2021-09-01
+        },
+        {
+            id: 15,
+            idUsuario: 5,
+            idPost: 1,
+            comentario: "Ufff que buen color!",
+            fechaCreacion: 2021-09-03
+        },
+        {
+            id: 16,
+            idUsuario: 4,
+            idPost: 1,
+            comentario: "Tienen imágenes del interior?",
+            fechaCreacion: 2021-09-03
+        },
+        {
+            id: 17,
+            idUsuario: 2,
+            idPost: 2,
+            comentario: "Que lindo auto!",
+            fechaCreacion: 2021-09-14
+        },
+        {
+            id: 18,
+            idUsuario: 1,
+            idPost: 2,
+            comentario: "Precio?",
+            fechaCreacion: 2021-09-14
+        },
+        {
+            id: 19,
+            idUsuario: 3,
+            idPost: 2,
+            comentario: "Que métodos de pago tienen?",
+            fechaCreacion: 2021-09-15
+        }
+    ]
+}
